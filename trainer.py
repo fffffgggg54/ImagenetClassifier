6dbf1685570b387b560e8093bef77391c9e07605
Fredo Guan <fredo.guan@hotmail.com>
2023-01-03 21:38:27 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -321,7 +321,7 @@ def trainCycle(image_datasets, model):
                         # backward + optimize only if in training phase
                         if phase == 'train' and (loss.isnan() == False):
                             accelerator.backward(loss)
-                            if(i % FLAGS['gradient_accumulation_iterations'] == 0):
+                            if((i+1) % FLAGS['gradient_accumulation_iterations'] == 0):
                                 nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0, norm_type=2)
                                 optimizer.step()
                                 optimizer.zero_grad()
