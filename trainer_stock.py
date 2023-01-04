dd1339b5494647cee4b796588cc72f6523a4ebd6
Fredo Guan <fredo.guan@hotmail.com>
2023-01-03 22:35:22 -0800

    Update trainer_stock.py

--- trainer_stock.py
+++ trainer_stock.py
@@ -261,7 +261,7 @@ def trainCycle(image_datasets, model):
                     scheduler.step()
                 
             if phase == 'val':
-                print(f'top-1: {100 * (correct/samples)}')
+                print(100 * (correct/samples))
                 
         time_elapsed = time.time() - epochTime
         print(f'epoch {epoch} completed in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
