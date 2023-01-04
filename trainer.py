1a27cde54df2e0c6d0a508896080bf5b5cc4d706
Fredo Guan <fredo.guan@hotmail.com>
2023-01-03 21:43:45 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -33,7 +33,7 @@ from timm.data.mixup import FastCollateMixup, Mixup
 
 from accelerate import Accelerator
 
-accelerator = Accelerator()
+accelerator = Accelerator(mixed_precision="bf16", downcast_bf16=True)
 
 
 # ================================================
@@ -69,7 +69,7 @@ FLAGS['num_workers'] = 35
 
 FLAGS['num_epochs'] = 100
 FLAGS['batch_size'] = 256
-FLAGS['gradient_accumulation_iterations'] = 4
+FLAGS['gradient_accumulation_iterations'] = 1
 
 FLAGS['base_learning_rate'] = 3e-3
 FLAGS['base_batch_size'] = 2048
