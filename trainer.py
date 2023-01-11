4123241ed4962c203a16ac90eeeed9e14867aa33
Fredo Guan <fredo.guan@hotmail.com>
2023-01-10 22:24:53 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -176,7 +176,7 @@ def modelSetup(classes):
     
     #model = timm.create_model('maxvit_tiny_tf_224.in1k', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('ghostnet_050', pretrained=True, num_classes=len(classes))
-    model = timm.create_model('convnext_base', pretrained=True, num_classes=len(classes))
+    model = timm.create_model('mobilevitv2_200', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('tf_efficientnetv2_b3', pretrained=False, num_classes=len(classes), drop_rate = 0.00, drop_path_rate = 0.0)
     
     #model = ml_decoder.add_ml_decoder_head(model)
