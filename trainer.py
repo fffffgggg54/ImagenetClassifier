387b6d06e04673b867bd8b6a38f4693e52832be7
Fredo Guan <fredo.guan@hotmail.com>
2023-01-10 18:18:41 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -277,8 +277,8 @@ def trainCycle(image_datasets, model):
             #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         
-        image_datasets['val'].transform = 
-            transforms.Compose([transforms.Resize((FLAGS['imageSize'],FLAGS['imageSize'])),
+        image_datasets['val'].transform = transforms.Compose([
+            transforms.Resize((FLAGS['imageSize'],FLAGS['imageSize'])),
             transforms.ToTensor(),
             #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
