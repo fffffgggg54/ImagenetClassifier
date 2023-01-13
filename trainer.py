de63d5a465f0ba9a93bd598bcb5109d156a24eac
Fredo Guan <fredo.guan@hotmail.com>
2023-01-13 14:39:35 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -218,7 +218,7 @@ def modelSetup(classes):
     
     #model = timm.create_model('maxvit_tiny_tf_224.in1k', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('ghostnet_050', pretrained=True, num_classes=len(classes))
-    model = timm.create_model('convnext_large.fb_in22k_ft_in1k_384', pretrained=False, num_classes=len(classes))
+    model = timm.create_model('convnext_large.fb_in22k_ft_in1k_384', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('edgenext_xx_small', pretrained=False, num_classes=len(classes))
     #model = timm.create_model('tf_efficientnetv2_b3', pretrained=False, num_classes=len(classes), drop_rate = 0.00, drop_path_rate = 0.0)
     
