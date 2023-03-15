7b08cb7e5d550c818bdec005ac53aaef39bc0834
Fredo Guan <fredo.guan@hotmail.com>
2023-03-14 21:26:14 -0700

    Update trainer_gpu.py

--- trainer_gpu.py
+++ trainer_gpu.py
@@ -88,7 +88,7 @@ FLAGS['num_workers'] = 20
 # training config
 
 FLAGS['num_epochs'] = 100
-FLAGS['batch_size'] = 16
+FLAGS['batch_size'] = 128
 FLAGS['gradient_accumulation_iterations'] = 8
 
 FLAGS['base_learning_rate'] = 1e-3
@@ -465,7 +465,7 @@ def modelSetup(classes):
     
     #model = timm.create_model('maxvit_tiny_tf_224.in1k', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('ghostnet_050', pretrained=True, num_classes=len(classes))
-    model = timm.create_model('identityformer_m48', pretrained=False, num_classes=len(classes))
+    model = timm.create_model('identityformer_s12', pretrained=False, num_classes=len(classes))
     #model = timm.create_model('vit_small_resnet26d_224', pretrained=False, num_classes=len(classes), drop_rate = 0., drop_path_rate = 0.1)
     #model = timm.create_model('lcnet_035', pretrained=False, num_classes=len(classes), drop_rate = 0.0, drop_path_rate = 0.)
     
