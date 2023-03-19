78545ff9fbcc5e9d4b9f5e8eb53550747475d2d0
Fredo Guan <fredo.guan@hotmail.com>
2023-03-19 09:55:04 -0700

    Update trainer_gpu.py

--- trainer_gpu.py
+++ trainer_gpu.py
@@ -211,7 +211,7 @@ def modelSetup(classes):
     #model = timm.create_model('convnext_base', pretrained=False, num_classes=len(classes))
     #model = timm.create_model('vit_small_resnet26d_224', pretrained=False, num_classes=len(classes), drop_rate = 0., drop_path_rate = 0.1)
     
-    model = timm.create_model('vit_small_patch32_224', pretrained=False, num_classes=0, drop_rate = 0.0, drop_path_rate = 0.2, global_pool='', class_token=False)
+    model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=0, drop_rate = 0.0, drop_path_rate = 0.2, global_pool='', class_token=False)
     model = I_JEPA(model)
     
 
