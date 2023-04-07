ec68029338e33550dc959e0088657146c3181bd8
Fredo Guan <fredo.guan@hotmail.com>
2023-04-07 15:40:19 -0700

    Update trainer_gpu.py

--- trainer_gpu.py
+++ trainer_gpu.py
@@ -89,8 +89,8 @@ FLAGS['num_workers'] = 10
 # training config
 
 FLAGS['num_epochs'] = 100
-FLAGS['batch_size'] = 128
-FLAGS['gradient_accumulation_iterations'] = 8
+FLAGS['batch_size'] = 64
+FLAGS['gradient_accumulation_iterations'] = 16
 
 FLAGS['base_learning_rate'] = 1e-3
 FLAGS['base_batch_size'] = 1024
