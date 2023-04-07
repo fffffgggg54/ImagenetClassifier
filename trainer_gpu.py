70d0f7caa8eb2390d0e54cd02fb256d355944924
Fredo Guan <fredo.guan@hotmail.com>
2023-04-07 15:49:34 -0700

    Update trainer_gpu.py

--- trainer_gpu.py
+++ trainer_gpu.py
@@ -66,7 +66,7 @@ FLAGS = {}
 FLAGS['rootPath'] = "/media/fredo/KIOXIA/Datasets/imagenet/"
 FLAGS['imageRoot'] = FLAGS['rootPath'] + 'data/'
 
-FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/IJEPA_vit_base_patch16_224/'
+FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/IJEPA_vit_base_patch32_224/'
 
 
 
@@ -211,7 +211,7 @@ def modelSetup(classes):
     #model = timm.create_model('convnext_base', pretrained=False, num_classes=len(classes))
     #model = timm.create_model('vit_small_resnet26d_224', pretrained=False, num_classes=len(classes), drop_rate = 0., drop_path_rate = 0.1)
     
-    model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=0, drop_rate = 0.0, drop_path_rate = 0.2, global_pool='', class_token=False)
+    model = timm.create_model('vit_base_patch32_224', pretrained=False, num_classes=0, drop_rate = 0.0, drop_path_rate = 0.2, global_pool='', class_token=False)
     model = I_JEPA(model)
     
 
